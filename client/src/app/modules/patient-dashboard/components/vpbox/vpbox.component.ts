a930ec7cc2ece9604313f04a01f6fa713c1945c8
luchu241 <luchu241@student.liu.se>
2021-10-04 11:17:04 +0200

    continued working on the vital parameter box, made it more generic, now it reads in icon, name and value from a list instead

--- client/src/app/modules/patient-dashboard/components/vpbox/vpbox.component.ts
+++ client/src/app/modules/patient-dashboard/components/vpbox/vpbox.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-vpbox',
   templateUrl: './vpbox.component.html',
-  styleUrls: ['./vpbox.component.scss']
+  styleUrls: ['./vpbox.component.scss'],
 })
 export class VpboxComponent implements OnInit {
+  params = [
+    { icon: 'folder', name: 'Pulse', value: '97 bpm' },
+    { icon: 'folder', name: 'Blood Pressure', value: '140/65 mmHg' },
+    { icon: 'folder', name: 'Temp.', value: '39°C' },
+    { icon: 'folder', name: 'Resp.-rate', value: '32 / min' },
+    { icon: 'folder', name: 'SpO2', value: '96%' },
+    { icon: 'folder', name: 'Hydration', value: 'normal' },
+    { icon: 'folder', name: 'Consciousness', value: 'C' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
