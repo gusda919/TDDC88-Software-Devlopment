5818d6be2123ff45a3befd92ee7dd117aa38f544
Erik Parö <eripa972@student.liu.se>
2021-10-06 14:50:01 +0200

    icons done, dialog not done

--- client/src/app/core/header/nav/nav.component.ts
+++ client/src/app/core/header/nav/nav.component.ts
@@ -10,6 +10,7 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class NavComponent {
 
+  date : number  = Date.now();
   menuItems = ['overview', 'dashboard', 'employees']
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Tablet, Breakpoints.Handset])
