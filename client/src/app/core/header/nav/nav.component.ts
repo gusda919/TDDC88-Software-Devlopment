cf2893968093745afd637f6a37f4f02b2901005a
Erik Parö <eripa972@student.liu.se>
2021-10-08 15:41:43 +0000

    Update nav.component.ts

--- client/src/app/core/header/nav/nav.component.ts
+++ client/src/app/core/header/nav/nav.component.ts
@@ -15,6 +15,13 @@ export class NavComponent {
   displayMessage = false;
   panelOpenState = false;
 
+  messages = [
+    {id: 0, patient: 'Test Testsson', pn:"981010-0110",  content: 'Febern har ökat till 43'},
+    {id: 1, patient: 'Exempel Sonsson', pn:"911212-0110",content: 'Patienten har svår buksmärta'},
+    {id: 2, patient: 'Sven Svensson', pn:"941212-0110",content: 'Blodprov är nu tillgängligt'}
+  ]
+
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Tablet, Breakpoints.Handset])
     .pipe(
       map(result => result.matches),
@@ -29,8 +36,15 @@ export class NavComponent {
       }
     }
 
+    deleteMessage(i:number) {
+      this.messages.forEach((value,index)=>{
+        if(value.id == i) this.messages.splice(index,1);
+    });
+    }
+
+    
+
+
   constructor(private breakpointObserver: BreakpointObserver) {}
-
-
 
 }
