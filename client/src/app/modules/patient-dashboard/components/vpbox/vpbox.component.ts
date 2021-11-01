7ce86d51adba957ee14cd6676145c8cb69779545
luchu241 <luchu241@student.liu.se>
2021-11-01 15:41:26 +0100

    working on displaying vital parameter values from real data

--- client/src/app/modules/patient-dashboard/components/vpbox/vpbox.component.ts
+++ client/src/app/modules/patient-dashboard/components/vpbox/vpbox.component.ts
@@ -1,59 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { VitalParameters } from 'src/app/shared/models/patient';
+
+import { PatientService } from '../../../../core/services/patient.service'
 
 @Component({
   selector: 'app-vpbox',
   templateUrl: './vpbox.component.html',
   styleUrls: ['./vpbox.component.scss'],
 })
+
 export class VpboxComponent implements OnInit {
-  params = [
-    { icon: 'folder', name: 'Pulse', value: '97 bpm', isGraphDisplayed: false },
-    {
-      icon: 'folder',
-      name: 'Blood Pressure',
-      value: '140/65 mmHg',
-      isGraphDisplayed: false,
-    },
-    { icon: 'folder', name: 'Temp.', value: '39°C', isGraphDisplayed: false },
-    {
-      icon: 'folder',
-      name: 'Resp.-rate',
-      value: '32 / min',
-      isGraphDisplayed: false,
-    },
-    { icon: 'folder', name: 'SpO2', value: '96%', isGraphDisplayed: false },
-    {
-      icon: 'folder',
-      name: 'Hydration',
-      value: 'normal',
-      isGraphDisplayed: false,
-    },
-    {
-      icon: 'folder',
-      name: 'Consciousness',
-      value: 'C',
-      isGraphDisplayed: false,
-    },
-  ];
 
-  constructor() {}
+  isLoaded = false;
+  patientId = "195001232296";
+  vitalParameters: VitalParameters;
 
-  ngOnInit(): void {}
+  constructor(private patientService: PatientService) {}
 
-  isDisplay = false;
-
-  print(param: any) {
-    //Kod som ser till att enbart en graf kan visas samtidigt, tas
-    //stycket nedan bort kan man visa flera grafer samtidigt
-    if (!param.isGraphDisplayed) {
-      for (let i = 0; i < this.params.length; i++) {
-        if (this.params[i].isGraphDisplayed) {
-          this.params[i].isGraphDisplayed = false;
-        }
-      }
-    }
-
-    //
-    param.isGraphDisplayed = !param.isGraphDisplayed;
+  ngOnInit(): void {
+    this.getVitalParameters();
+    
   }
+
+  getVitalParameters() {
+    this.patientService.getPatientVitalparameters(this.patientId).subscribe((vitalParameters: VitalParameters) => {this.vitalParameters = vitalParameters, this.isLoaded = true, console.log(vitalParameters)});
+  }
+
 }
