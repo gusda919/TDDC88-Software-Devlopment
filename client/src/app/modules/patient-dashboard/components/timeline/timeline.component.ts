697450c4bfdff372965896d0c3e7c22935641e64
Jacob Karlén <jacka782@student.liu.se>
2021-11-09 19:08:38 +0100

    feat: add simple timeline component with horizontal scroll

--- client/src/app/modules/patient-dashboard/components/timeline/timeline.component.ts
+++ client/src/app/modules/patient-dashboard/components/timeline/timeline.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timeline.component.scss']
 })
 export class TimelineComponent implements OnInit {
+  
+  displayedEvent: any = {};
+
+  events: any = [
+    {
+      "date": "2021-01-20",
+      "time": "18:05",
+      "description": "Fick en smörmåg och kaffe"
+    },
+    {
+      "date": "2021-01-21",
+      "time": "14:05",
+      "description": "Provresultat kom tillbaka"
+    },
+    {
+      "date": "2021-01-21",
+      "time": "16:05",
+      "description": "Skickades till röntgen för att ta bilder"
+    },
+    {
+      "date": "2021-01-22",
+      "time": "18:05",
+      "description": "Blev utskriven från avdelningen"
+    },
+    {
+      "date": "2021-01-22",
+      "time": "18:05",
+      "description": "Blev utskriven från avdelningen"
+    },
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  displayEvent(event: any) {
+    this.displayedEvent = event;
   }
 
 }
