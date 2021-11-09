16394195ed149386c25ab30e3fa690fe9656405f
Jacob Karlén <jacka782@student.liu.se>
2021-11-09 09:40:05 +0100

    refactor: refactor updateTriageColor of patient-header

--- client/src/app/modules/patient-dashboard/components/patient-header/patient-header.component.ts
+++ client/src/app/modules/patient-dashboard/components/patient-header/patient-header.component.ts
@@ -35,26 +35,15 @@ export class PatientHeaderComponent implements OnInit{
       this.triage = cont.triage,
       this.visitingFor = cont.description.substr(cont.description.indexOf(" ") + 1).charAt(0).toUpperCase() + cont.description.substr(cont.description.indexOf(" ") + 1).slice(1),
       this.gender = cont.gender.charAt(0).toUpperCase() + cont.gender.slice(1)
+      this.updateTriageColor();
     });
 
-    this.updateTriageColor();
+    
   }
 
   updateTriageColor(){
-
-    // refactor idea to avoid unneeded code
-    // triageColor?.setAttribute('style', ('background-color: ' + this.triage.toLowerCase()));
-
     let triageColor = document.getElementById('triageColor')
-    if (this.triage === 'grön') {
-      triageColor?.setAttribute('style', 'background-color: green');
-    } else if (this.triage === 'gul') {
-      triageColor?.setAttribute('style', 'background-color: yellow');
-    } else if (this.triage === 'orange') {
-      triageColor?.setAttribute('style', 'background-color: orange');
-    } else if (this.triage === 'röd') {
-      triageColor?.setAttribute('style', 'background-color: red');
-    }
+    triageColor?.setAttribute('style', 'background-color: ' + this.triage);
   }
 }
 
