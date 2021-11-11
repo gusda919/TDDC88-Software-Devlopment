40856d949e2fefa5527bfb0961b71da7979ae074
luchu241 <luchu241@student.liu.se>
2021-11-11 14:28:50 +0100

    feat: timeline that displays different events relevant for the patient (WIP)

--- client/src/app/modules/patient-dashboard/components/timeline/timeline.component.ts
+++ client/src/app/modules/patient-dashboard/components/timeline/timeline.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PatientService } from 'src/app/core/services/patient.service';
-import { Caregiving } from 'src/app/shared/models/patient';
+import { Caregiving, Lab, Drug } from 'src/app/shared/models/patient';
 
 
 
@@ -9,9 +9,7 @@ interface TimelineEvent {
   date: Date,
   icon: string,
   type: string,
-  data: {
-    note: string,
-  }
+  data: any,
 }
 
 
@@ -29,9 +27,9 @@ export class TimelineComponent implements OnInit {
   @Input() patientId: string;
 
   caregiving: Caregiving[];
-
+  labs: Lab[];
+  drugs: Drug[];
   events: TimelineEvent[] = [];
-
 
 
   constructor(private patientService: PatientService) { }
@@ -41,14 +39,52 @@ export class TimelineComponent implements OnInit {
     this.patientService.getPatientCaregiving(this.patientId).subscribe((caregiving: Caregiving[]) => {      
       let events: TimelineEvent[] = caregiving.map((c: Caregiving): TimelineEvent => ({
         date: new Date(c.date+"T"+c.time),
-        icon: 'caregiving-icon',
+        icon: 'event',
         type: 'caregiving',
+        data: c.note
+      })
+      );
+      this.events = this.events.concat(events);
+      this.getPatientLabs();
+    });
+
+  }
+
+  getPatientLabs() {
+    this.patientService.getPatientLabs(this.patientId).subscribe((labs: Lab[]) => {      
+      let events: TimelineEvent[] = labs.map((l: Lab): TimelineEvent => ({
+        date: new Date(l.date+"T"+l.time),
+        icon: 'event',
+        type: 'lab',
+        data: l.tests,
+      })
+      );
+      this.events = this.events.concat(events); 
+      this.getPatientDrugs();    
+    });
+  }
+
+  getPatientDrugs() {
+    this.patientService.getPatientDrugs(this.patientId).subscribe((drugs: Drug[]) => {      
+      let events: TimelineEvent[] = drugs.map((d: Drug): TimelineEvent => ({
+        date: new Date(d.date+"T"+d.time),
+        icon: 'event',
+        type: 'drug',
         data: {
-          note: c.note,
+          type: d.type,
+          dose: d.dose
         }
       })
       );
-      this.events = this.events.concat(events);    
+      this.events = this.events.concat(events);
+      console.log(this.events);
+      this.sortData(); 
+    });
+  }
+
+  sortData() {
+    return this.events.sort((a: TimelineEvent, b: TimelineEvent) => {
+      return <any>new Date(a.date) - <any>new Date(b.date);
     });
   }
 
