84a61e9b341f1d6c796abaf34bd10e06a6f5b780
Adam Lilja <adali126@student.liu.se>
2021-11-15 11:38:33 +0100

    started trying to fix sorting

--- client/src/app/modules/patients-overview/components/overview-table/overview-table.component.ts
+++ client/src/app/modules/patients-overview/components/overview-table/overview-table.component.ts
@@ -1,10 +1,11 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
 
 import { PatientService } from 'src/app/core/mocks/patient.service';
 import { Patient } from 'src/app/core/mocks/patient';
+import { DataSource } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-overview-table',
@@ -32,8 +33,10 @@ export class OverviewTableComponent implements AfterViewInit {
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['issue', 'name', 'id' ,'date', 'triage'];
   groupedColumns =['header'];
+  sortedData: Patient[];
 
   constructor(private patientService: PatientService) {
+    this.sortedData = this.dataSource.data;
   }
 
   ngAfterViewInit(): void {
@@ -49,4 +52,39 @@ export class OverviewTableComponent implements AfterViewInit {
     });
 
   }
+
+  sortData(sort:Sort) {
+    const data = this.dataSource.data.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+    this.sortedData = data.sort((a, b) => {
+      console.log(a);
+      console.log(b);
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.last_name, b.last_name, isAsc);
+        case 'issue':
+          return compare(a.visiting_for, b.visiting_for, isAsc);
+        case 'id':
+          return compare(a.ssn, b.ssn, isAsc);
+        case 'date':
+          return compare(a.visiting_date, b.visiting_date, isAsc);
+        
+        default:
+          return 0;
+
+      }
+    });
+
+
+  }
+
+  
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
