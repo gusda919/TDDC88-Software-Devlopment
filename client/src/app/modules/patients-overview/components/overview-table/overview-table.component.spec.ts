9198ae1da8b43aaef7238192be546e35b68a96e2
Hannes Ekstam Ljusegren <hanek737@student.liu.se>
2021-11-16 22:40:31 +0100

    Removed Dashboard title from dashboard

--- client/src/app/modules/patients-overview/components/overview-table/overview-table.component.spec.ts
+++ client/src/app/modules/patients-overview/components/overview-table/overview-table.component.spec.ts
@@ -1,6 +1,7 @@
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorIntl} from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 
@@ -32,3 +33,30 @@ describe('OverviewTableComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+const SwedishRangeLabel = (page: number, pageSize: number, length: number) => {
+  if (length == 0 || pageSize == 0) { return `0 till ${length}`; }
+  
+  length = Math.max(length, 0);
+
+  const startIndex = page * pageSize;
+
+  // If the start index exceeds the list length, do not try and fix the end index to the end.
+  const endIndex = startIndex < length ?
+      Math.min(startIndex + pageSize, length) :
+      startIndex + pageSize;
+
+  return `${startIndex + 1} - ${endIndex} rill ${length}`;
+}
+
+
+export function getSwedishPaginatorIntl() {
+  const paginatorIntl = new MatPaginatorIntl();
+  
+  paginatorIntl.itemsPerPageLabel = 'Patienter per sida:';
+  paginatorIntl.nextPageLabel = 'Nästa sida';
+  paginatorIntl.previousPageLabel = 'Föregående sida';
+  paginatorIntl.getRangeLabel = SwedishRangeLabel;
+  
+  return paginatorIntl;
+}
