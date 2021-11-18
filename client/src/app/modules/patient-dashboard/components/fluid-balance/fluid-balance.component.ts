8709d877efea9c01d1691ee7563cc385d8a8b517
Jacob Karlén <jacka782@student.liu.se>
2021-11-18 18:53:36 +0100

    feat: add waterfall chart of fluid balance

--- client/src/app/modules/patient-dashboard/components/fluid-balance/fluid-balance.component.ts
+++ client/src/app/modules/patient-dashboard/components/fluid-balance/fluid-balance.component.ts
@@ -13,27 +13,91 @@ export class FluidBalanceComponent implements OnInit {
   @Input()
   patientId: string;
 
-  data: [];
-  labels: [];
+  data: number[][] = [];
+  labels: string[] = [];
+  backgroundColors: string[] = [];
+
+  waterfall: Chart;
 
   constructor(private patientService: PatientService) { }
 
   ngOnInit(): void {
     this.getChartData();
+
   }
 
-  getChartData() {
+  getChartData() { 
     this.patientService.getPatientFluidBalance(this.patientId).subscribe((fb: FluidBalance) => {
+      let total: number = 0;
 
       fb.in.forEach((fbd: FluidBalanceData) => {
-
+        let vStart: number = total;
+        total += fbd.value;
+        this.data.push([vStart, total]);
+        this.labels.push(fbd.label);
+        this.backgroundColors.push('rgb(76, 175, 80)');
       })
 
       fb.out.forEach((fbd: FluidBalanceData) => {
-
+        let vStart: number = total;
+        total -= fbd.value;
+        this.data.push([vStart, total]);
+        this.labels.push(fbd.label);
+        this.backgroundColors.push('rgb(229, 57, 53)');
       })
-   
-    })
+
+      this.data.push([0, total]);
+      this.labels.push("Skillnad");
+      this.backgroundColors.push('rgb(255, 193, 7)')
+      
+      this.renderChart();
+      console.log(this.data);
+    });
+  }
+
+  renderChart() {
+    this.waterfall = new Chart('waterfall', {
+      type: 'bar',
+      data: {
+        labels: this.labels,
+        datasets: [{
+          data: this.data,
+          backgroundColor: this.backgroundColors,
+          barPercentage: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false,
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        },
+        plugins: {
+          datalabels: {
+            anchor: 'end',
+            align: 'end'
+          },
+          tooltips: {
+            callbacks: {
+              label: (tooltipItem: any) => {
+                let v = tooltipItem.index ? this.data[tooltipItem.index][1] - this.data[tooltipItem.index][0] : 0;
+                console.log(v);
+                return v;
+              }
+            }
+          },
+        }
+      }
+    });
+
   }
 
 }
+
