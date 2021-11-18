5b135b96a1e851aac9a880fe0f8e4b9a4fa3b8ad
Erik Parö <eripa972@student.liu.se>
2021-11-18 11:09:34 +0000

    Update karma.conf.js

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['Headless'],
     customLaunchers: {
       Headless: {
         base: 'ChromeHeadless',
