cf6c74add6bb640c58ce42163f5b95adbe88c7f5
Jacob Karlén <jacka782@student.liu.se>
2021-11-18 10:44:51 +0100

    feat: add fluid balance component

---
+++ client/src/app/modules/patient-dashboard/components/fluid-balance/fluid-balance.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fluid-balance',
+  templateUrl: './fluid-balance.component.html',
+  styleUrls: ['./fluid-balance.component.scss']
+})
+export class FluidBalanceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
