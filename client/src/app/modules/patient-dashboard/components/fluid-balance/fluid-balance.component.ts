8ff0be6d3c6f9700cf8f8278e29f46961b412246
Jacob Karlén <jacka782@student.liu.se>
2021-11-18 14:12:04 +0100

    feat: add data retrieval to fluid balance component

--- client/src/app/modules/patient-dashboard/components/fluid-balance/fluid-balance.component.ts
+++ client/src/app/modules/patient-dashboard/components/fluid-balance/fluid-balance.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Chart } from "chart.js";
+import { PatientService } from 'src/app/core/services/patient.service';
+import { FluidBalance, FluidBalanceData } from 'src/app/shared/models/patient';
 
 @Component({
   selector: 'app-fluid-balance',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FluidBalanceComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  patientId: string;
+
+  data: [];
+  labels: [];
+
+  constructor(private patientService: PatientService) { }
 
   ngOnInit(): void {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.patientService.getPatientFluidBalance(this.patientId).subscribe((fb: FluidBalance) => {
+
+      fb.in.forEach((fbd: FluidBalanceData) => {
+
+      })
+
+      fb.out.forEach((fbd: FluidBalanceData) => {
+
+      })
+   
+    })
   }
 
 }
