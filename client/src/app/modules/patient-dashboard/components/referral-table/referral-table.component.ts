b34092b50d59661a013b26c403ee70ed9bed0fd3
Adam Lilja <adali126@student.liu.se>
2021-11-25 09:34:12 +0100

    fix merge  conflict

--- client/src/app/modules/patient-dashboard/components/referral-table/referral-table.component.ts
+++ client/src/app/modules/patient-dashboard/components/referral-table/referral-table.component.ts
@@ -42,26 +42,26 @@ export class ReferralTableComponent implements AfterViewInit {
   getPatientReferrals() {
     if (this.patientId=='195001232296') {
       let data: ReferralTableItem[] = [ 
-        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1"},
-        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2"},
-        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3"},
-        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4"},
+        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1 för patient med persnum: 195001232296"},
+        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2 för patient med persnum: 195001232296"},
+        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3 för patient med persnum: 195001232296"},
+        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4 för patient med persnum: 195001232296"},
       ];
       return data;
     } else if (this.patientId=='198605119885') {
       let data: ReferralTableItem[] = [ 
-        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1"},
-        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2"},
-        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3"},
-        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4"},
+        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1 för patient med persnum: 198605119885"},
+        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2 för patient med persnum: 198605119885"},
+        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3 för patient med persnum: 198605119885"},
+        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4 för patient med persnum: 198605119885"},
       ];
       return data;
     } else if (this.patientId=='194202269207') {
       let data: ReferralTableItem[] = [ 
-        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1"},
-        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2"},
-        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3"},
-        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4"},
+        {date: "2021-11-15 13:21", updated: "2021-11-16 08:31", type: "Remiss 1", unit: "Enhet 1", answered: "2021-11-17 15:26", description: "Utförlig information 1 för patient med persnum: 194202269207"},
+        {date: "2021-11-16 13:20", updated: "2021-11-17 08:32", type: "Remiss 2", unit: "Enhet 2", answered: "2021-11-18 15:27", description: "Utförlig information 2 för patient med persnum: 194202269207"},
+        {date: "2021-11-17 13:22", updated: "2021-11-18 08:33", type: "Remiss 3", unit: "Enhet 3", answered: "2021-11-19 15:28", description: "Utförlig information 3 för patient med persnum: 194202269207"},
+        {date: "2021-11-18 13:23", updated: "2021-11-19 08:34", type: "Remiss 4", unit: "Enhet 4", answered: "2021-11-20 15:29", description: "utförlig information 4 för patient med persnum: 194202269207"},
       ];
       return data;
     } else {
