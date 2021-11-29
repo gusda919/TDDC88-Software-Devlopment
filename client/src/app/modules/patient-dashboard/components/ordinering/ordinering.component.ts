a28cc97a51a16a5e8c8c9574bc113d31836c218c
Hannes Ekstam Ljusegren <hanek737@student.liu.se>
2021-11-29 23:47:50 +0100

    Medicinering widget done hardcoded

--- client/src/app/modules/patient-dashboard/components/ordinering/ordinering.component.ts
+++ client/src/app/modules/patient-dashboard/components/ordinering/ordinering.component.ts
@@ -16,4 +16,21 @@ export class OrdineringComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  showMore(){
+    document.getElementById("wrapper-base")!.style.display = "none";
+    document.getElementById("wrapper-extended")!.style.display = "block";
+  }
+  showHistory(){
+
+    document.getElementById("wrapper-base")!.style.display = "none";
+    document.getElementById("wrapper-history")!.style.display = "block";
+  }
+  showLess(){
+
+    document.getElementById("wrapper-base")!.style.display = "block";
+    document.getElementById("wrapper-extended")!.style.display = "none";
+    document.getElementById("wrapper-history")!.style.display = "none"
+  }
+ 
+
 }
