d792715986999f533288fe20d8fd96684573c750
oscar <osclo731@student.liu.se>
2021-11-30 10:27:28 +0100

    fix errors

--- client/src/app/core/services/patient.service.spec.ts
+++ client/src/app/core/services/patient.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { PatientService } from './patient.service';
 
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient} from '@angular/common/http';
+
 describe('PatientService', () => {
   let service: PatientService;
 
--- client/src/app/core/services/user.service.spec.ts
+++ client/src/app/core/services/user.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient} from '@angular/common/http';
+
 describe('UserService', () => {
   let service: UserService;
 
